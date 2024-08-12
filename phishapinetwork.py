1b10896469cd3ed0b093c3a3a6d91a389d54fc16
eschultze <eduardo.schultze@gmail.com>
2024-08-12 15:06:40 -0300

    Update phishapinetwork.py
    Script rewrite
    New color scheme
    Add _size as parameter

--- phishapinetwork.py
+++ phishapinetwork.py
@@ -2,172 +2,147 @@
 # encoding=utf8
 import json
 import os
-import pathlib
 import requests
 import sys
 import webbrowser
-
-print("Phishstats.info API Network - v0.3")
-
-#Cheking for arguments
-if len(sys.argv) <= 3:
-	print("Not enough arguments to make search")
-	exit(1)
-
-column = sys.argv[1]
-compare = sys.argv[2]
-search = sys.argv[3]
-
-#Making the API request
-r = requests.get(
-	'https://phishstats.info:2096/api/phishing',
-	params = {'_where':'('+ column +','+ compare +','+ search +')', '_sort':'-id', '_size':'100'},
-	headers = {'User-Agent':'github-network-api'}
-)
-
-#Print the request URL
-#print(r.url)
-
-#Loading the output as json
-output = json.loads(r.text)
-
-print ("Query took " + str(r.elapsed.total_seconds()) + " seconds")
-
-url_counter = 0
-for url in output:
-	url_counter += 1
-
-#Cheking for data
-if url_counter <= 0:
-	print("Not enough data to create network")
-	exit(1)
-
 from pyvis.network import Network
 
-#Parameters for pyvis network
-g = Network(height="700px", width="700px", bgcolor="#718487", font_color="white")
+def print_usage():
+    print("Usage: phishapinetwork.py <column> <compare> <search> [size]")
+    print("Examples:")
+    print("phishapinetwork.py title eq facebook")
+    print("phishapinetwork.py title like ~facebook~")
+    print("phishapinetwork.py ip eq 148.228.16.3")
+    print("phishapinetwork.py tld eq br")
+    print("phishapinetwork.py countrycode eq br")
+    print("phishapinetwork.py title asn eq as13335")
+    print("phishapinetwork.py title isp like ~cloudflare~")
+    print("phishapinetwork.py title like ~brandyouwanttofind~")
+    print("phishapinetwork.py url like ~brandyouwanttofind~")
+    exit(1)
 
-#Uncomment below to force a specific algorithm
-#g.barnes_hut()
-#g.force_atlas_2based()
+def validate_input(args):
+    if len(args) < 4 or len(args) > 5:
+        print("Invalid number of arguments to make search")
+        print_usage()
 
-#Comment below if you don't want physics options on the final html
-g.show_buttons(filter_=['physics'])
+def fetch_data(column, compare, search, size):
+    try:
+        # Setting the timeout to 20 seconds
+        r = requests.get(
+            'https://phishstats.info:2096/api/phishing',
+            params={'_where': f'({column},{compare},{search})', '_sort': '-id', '_size': size},
+            headers={'User-Agent': 'github-network-api'},
+            timeout=20  # Timeout set to 20 seconds
+        )
+        r.raise_for_status()
+        return r.json(), r.elapsed.total_seconds()
+    except requests.exceptions.Timeout:
+        if int(size) > 10:
+            print("Request timed out. Reducing size to 10 and trying again...")
+            # Retry with size 10
+            try:
+                r = requests.get(
+                    'https://phishstats.info:2096/api/phishing',
+                    params={'_where': f'({column},{compare},{search})', '_sort': '-id', '_size': 10},
+                    headers={'User-Agent': 'github-network-api'}
+                )
+                r.raise_for_status()
+                return r.json(), r.elapsed.total_seconds()
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching data after retry: {e}")
+                exit(1)
+        else:
+            print(f"Request timed out. No retry because size is already 10 or less.")
+            exit(1)
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        exit(1)
 
-url_number = 1
+def create_network_graph(data):
+    g = Network(height="700px", width="1200px", bgcolor="#282a36", font_color="#f8f8f2")
+    g.show_buttons(filter_=['physics'])
 
-for entry in output:
+    url_number = 1
+    for entry in data:
+        add_nodes_and_edges(g, entry, url_number)
+        url_number += 1
 
-	new_url = entry['url']
-	new_url = '{}'.format(new_url)
-	new_url_http_code = entry['http_code']
-	new_url_http_server = entry['http_server']
-	new_url_safebrowsing = entry['google_safebrowsing']
-	new_url_technology = entry['technology']
-	if new_url != "":
-		g.add_node("URL-" + str(url_number), title = (new_url + "<br> HTTP code: " + str(new_url_http_code) + "<br> HTTP server: " + str(new_url_http_server) + "<br> Safebrowsing: " + str(new_url_safebrowsing) + "<br> Technologies:" + str(new_url_technology)), color = "#1ba1e2")
+    neighbor_map = g.get_adj_list()
+    for node in g.nodes:
+        node["value"] = len(neighbor_map[node["id"]])
 
-	new_title = entry['title']
-	new_title = '{}'.format(new_title)
-	if new_title != "":
-		g.add_node(new_title, color = "#2eaf57")
+    return g
 
-	if new_url != "":
-		if new_title != "":
-			g.add_edge("URL-" + str(url_number), new_title)
+def add_nodes_and_edges(g, entry, url_number):
+    new_url = entry.get('url', '')
+    new_title = entry.get('title', '')
+    new_ip = entry.get('ip', '')
+    new_host = entry.get('host', '')
+    new_domain = entry.get('domain', '')
+    new_asn = entry.get('asn', '')
+    new_isp = entry.get('isp', '')
+    new_countryname = entry.get('countryname', '')
 
-	new_ip = entry['ip']
-	new_ip = '{}'.format(new_ip)
+    if new_url:
+        g.add_node(f"URL-{url_number}", title=f"{new_url}<br> HTTP code: {entry.get('http_code', '')}<br> HTTP server: {entry.get('http_server', '')}<br> Safebrowsing: {entry.get('google_safebrowsing', '')}<br> Technologies: {entry.get('technology', '')}", color="#ff79c6")
 
-	if new_ip != "":
-		new_ip_times = entry['n_times_seen_ip']
-		new_ip_vulns = entry['vulns']
-		new_ip_ports = entry['ports']
-		new_ip_tags = entry['tags']
-		new_ip_os = entry['os']
-		new_ip_abusech = entry['abuse_ch_malware']
-		g.add_node(new_ip, title = ("N times seen IP: " + str(new_ip_times) + "<br> Vulnerabilities: " + str(new_ip_vulns) + "<br> Ports: " + str(new_ip_ports) + "<br> Tags: " + str(new_ip_tags) + "<br> OS: " + str(new_ip_os) + "<br> Abuse.ch (Malware): " + str(new_ip_abusech)), color = "#006699")
+    if new_title:
+        g.add_node(new_title, color="#bd93f9")
+        g.add_edge(f"URL-{url_number}", new_title)
 
-	if new_url != "":
-		if new_ip != "":
-			#if re.match (r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", new_ip):
-			g.add_edge("URL-" + str(url_number), new_ip)
+    if new_ip:
+        g.add_node(new_ip, title=f"N times seen IP: {entry.get('n_times_seen_ip', '')}<br> Vulnerabilities: {entry.get('vulns', '')}<br> Ports: {entry.get('ports', '')}<br> Tags: {entry.get('tags', '')}<br> OS: {entry.get('os', '')}<br> Abuse.ch (Malware): {entry.get('abuse_ch_malware', '')}", color="#8be9fd")
+        g.add_edge(f"URL-{url_number}", new_ip)
 
-	new_host = entry['host']
-	new_host = '{}'.format(new_host)
-	if new_host != "":
-		if new_host != new_ip:
-			new_host_times = entry['n_times_seen_host']
-			new_host_alexa = entry['alexa_rank_host']
-			new_host_abusech = entry['abuse_ch_malware']
-			g.add_node(new_host, title = ("N times seen Hostname: " + str(new_host_times) + "<br> Alexa rank: " + str(new_host_alexa) + "<br> Abuse.ch (Malware): " + str(new_host_abusech)), color = "#008080")
+    if new_host and new_host != new_ip:
+        g.add_node(new_host, title=f"N times seen Hostname: {entry.get('n_times_seen_host', '')}<br> Alexa rank: {entry.get('alexa_rank_host', '')}<br> Abuse.ch (Malware): {entry.get('abuse_ch_malware', '')}", color="#50fa7b")
+        g.add_edge(f"URL-{url_number}", new_host)
 
-	if new_url != "":
-		if new_host != "":
-			g.add_edge("URL-" + str(url_number), new_host)
+    if new_domain and new_domain != "None" and new_domain != new_host:
+        g.add_node(new_domain, title=f"N times seen Domain: {entry.get('n_times_seen_domain', '')}<br> Domain registered N days ago: {entry.get('domain_registered_n_days_ago', '')}<br> Alexa rank: {entry.get('alexa_rank_domain', '')}<br> Virustotal: {entry.get('virus_total', '')}<br> ThreatCrowd: {entry.get('threat_crowd', '')}<br> ThreatCrowd votes: {entry.get('threat_crowd_votes', '')}", color="#ffb86c")
+        g.add_edge(new_host, new_domain)
 
-	new_domain = entry['domain']
-	new_domain = '{}'.format(new_domain)
-	if new_domain != "":
-		if new_domain != "None":
-			if new_host != new_domain:
-				new_domain_times = entry['n_times_seen_domain']
-				new_domain_days_ago = entry['domain_registered_n_days_ago']
-				new_domain_alexa = entry['alexa_rank_domain']
-				new_domain_virustotal = entry['virus_total']
-				new_domain_threat_crowd = entry['threat_crowd']
-				new_domain_threat_crowd_votes = entry['threat_crowd_votes']
-				g.add_node(new_domain, title = ("N times seen Domain: " + str(new_domain_times) + "<br> Domain registered N days ago: " + str(new_domain_days_ago) + "<br> Alexa rank: " + str(new_domain_alexa) + "<br> Virustotal: " + str(new_domain_virustotal) + "<br> ThreatCrowd: " + str(new_domain_threat_crowd) + "<br> ThreatCrowd votes: " + str(new_domain_threat_crowd_votes)), color = "#eed514")
+    if new_asn:
+        g.add_node(new_asn, color="#ff5555")
+        g.add_edge(new_asn, new_ip)
 
-	if new_domain != "":
-		if new_host != "":
-			if new_domain != "None":
-				if new_domain != new_host:
-					g.add_edge(new_host, new_domain)
+    if new_isp:
+        g.add_node(new_isp, color="#f1fa8c")
+        g.add_edge(new_asn, new_isp)
 
-	new_asn = entry['asn']
-	new_asn = '{}'.format(new_asn)
-	if new_asn != "":
-		g.add_node(new_asn, color = "#db870e")
+    if new_countryname:
+        g.add_node(new_countryname, color="#bd93f9")
+        g.add_edge(new_countryname, new_isp)
 
-	if new_asn != "":
-		if new_ip != "":
-			g.add_edge(new_asn, new_ip)
+def save_and_open_graph(g, column, compare, search):
+    filename = f"{column}_{compare}_{search}_network.html"
+    g.show(filename)
+    print(f"File generated: {filename}")
+    webbrowser.open(f"file://{os.getcwd()}/{filename}", new=2)
 
-	new_isp = entry['isp']
-	new_isp = '{}'.format(new_isp)
-	if new_isp != "":
-		g.add_node(new_isp, color = "#7969a9")
+def main():
+    print("Phishstats.info API Network - v0.4")
+    validate_input(sys.argv)
 
-	if new_isp != "":
-		if new_asn != "":
-			g.add_edge(new_asn, new_isp)
+    column, compare, search = sys.argv[1:4]
+    size = sys.argv[4] if len(sys.argv) == 5 else "100"
+    data, query_time = fetch_data(column, compare, search, size)
 
-	new_countryname = entry['countryname']
-	new_countryname = '{}'.format(new_countryname)
-	if new_countryname != "":
-		g.add_node(new_countryname, color = "#39af8e")
+    print(f"Query took {query_time} seconds")
 
-	if new_countryname != "":
-		if new_isp != "":
-			g.add_edge(new_countryname, new_isp)
+    if not data:
+        print("Not enough data to create network")
+        exit(1)
 
-	url_number += 1
+    g = create_network_graph(data)
 
-#Checking for size/value of nodes
-neighbor_map = g.get_adj_list()
-for node in g.nodes:
-	node["value"] = len(neighbor_map[node["id"]])
+    print(f"URLs: {len(data)}")
+    print(f"Nodes: {g.num_nodes()}")
+    print(f"Edges: {g.num_edges()}")
 
-print("URLs: " + str(url_counter))
-print("Nodes: " + str(g.num_nodes()))
-print("Edges: " + str(g.num_edges()))
+    save_and_open_graph(g, column, compare, search)
 
-#g.show("network.html")
-g.show(column + "_" + compare + "_" + search + "_network.html")
-print("File generated: " + column + "_" + compare + "_" + search + "_network.html")
-
-#If you don't want to automatically open the current html file in your browser comment the 3 lines below
-new_tab = 2
-file = "file://" + os.getcwd() + "/" + column + "_" + compare + "_" + search + "_network.html"
-webbrowser.open(file,new=new_tab)
+if __name__ == "__main__":
+    main()
+(myenv)
